2c22dd29ed65d871453a4b2b587a73af90fd132a
Rahul Chauhan <omrahulchauhan@gmail.com>
2021-02-19 15:29:30 +0530

    modified main to add functions to use jsonrw and filerw

---
+++ go_grpc/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"go_grpc/filerw"
+	"go_grpc/jsonrw"
+	simplepb "go_grpc/source/simple"
+	"log"
+)
+
+func main() {
+	sm := filerw.CreateSimple()
+	// readWriteFile(sm)
+	readWriteJSON(sm)
+
+}
+
+func readWriteJSON(sm *simplepb.SimpleMessage) {
+	fmt.Println("Convert to and from a JSON string")
+
+	jsonString := jsonrw.ToJSON(sm)
+	fmt.Println("JSON String Converted From Proto Message :  ", string(jsonString))
+
+	sm2 := &simplepb.SimpleMessage{}
+	jsonrw.FromJSON(jsonString, sm2)
+	fmt.Println("Proto Message Converted From JSON String :  ", sm2)
+
+}
+
+func readWriteFile(sm *simplepb.SimpleMessage) {
+	fmt.Println("Read and Write to and from a Binary File")
+
+	if err := filerw.WriteToFile("simple.bin", sm); err != nil {
+		log.Fatalln("Writing to file failed", err)
+	}
+
+	sm2 := &simplepb.SimpleMessage{}
+	if err2 := filerw.ReadFromFile("simple.bin", sm2); err2 != nil {
+		log.Fatalln("Reading from file failed", err2)
+	}
+
+	fmt.Println(sm)
+	fmt.Println(sm2)
+}
